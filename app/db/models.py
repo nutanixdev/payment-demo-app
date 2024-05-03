ce354332c97e6078d3f2d2f844516329c4eee82a
Laura Jordana <laura@nutanix.com>
2024-05-03 09:03:58 -0700

    simple UI and add payee field

--- app/db/models.py
+++ app/db/models.py
@@ -9,6 +9,7 @@ class Payment(Base):
     id = Column(Integer, primary_key=True, index=True)
     amount = Column(Float(precision=2), nullable=False)
     currency = Column(String(3), nullable=False)
+    payee = Column(String(200), nullable=False)
     description = Column(String(200))
 
     def __repr__(self) -> str:
--- app/main.py
+++ app/main.py
@@ -3,7 +3,12 @@ from typing import Callable
 from fastapi import FastAPI, Depends, HTTPException, Request, Response, Body
 from fastapi.exceptions import RequestValidationError
 from fastapi.routing import APIRoute
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import httpx
 
 from db import models, schemas
 from db.crud import PaymentCrud
@@ -34,6 +39,8 @@ app.router.route_class = ValidationErrorLoggingRoute
 
 models.Base.metadata.create_all(bind=engine)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.post("/payments/", tags=["Payment"], response_model=schemas.Payment, status_code=201)
 async def create_payment(payment_request: schemas.PaymentCreate = Body(), db: Session = Depends(get_db)):
@@ -42,6 +49,30 @@ async def create_payment(payment_request: schemas.PaymentCreate = Body(), db: Se
     """
 
     return await PaymentCrud.create(db=db, payment=payment_request)
+
+# index page, welcome
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(
+        name="index.html", context={"request": request}
+    )
+
+class FormData(BaseModel):
+    payee: str
+    currency: str
+    amount: float
+    description: str
+
+@app.post("/process_form_data")
+async def process_form_data(form_data: FormData):
+    print(form_data.dict())
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post("http://localhost:8000/payments/", json=form_data.dict())
+            response.raise_for_status()
+            return JSONResponse(content={"message": "Form data processed successfully"})
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code=e.response.status_code, detail=str(e))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
